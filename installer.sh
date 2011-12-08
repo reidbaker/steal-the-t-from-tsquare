301f4fde1f81a4bb9b2f6f2f12121b3add0a32b8
Reid Baker <hamiltonbaker@yahoo.com>
2011-12-08 02:46:09 -0500

    remove flag taht causes the tar command to fail

--- installer.sh
+++ installer.sh
@@ -1,7 +1,7 @@
 ## This file assumes you are using backtrack 5R1 ##
 ###################################################
 echo "extracting sslstrip"
-tar -xfc sslstrip-0.9.tar.gz
+tar -xf  sslstrip-0.9.tar.gz
 cd 'sslstrip-0.9/'
 echo "installing sslstrip"
 python setup.py install
